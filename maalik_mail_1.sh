6737c9a815104fc115095b02558c007a814c76eb
maalik <ritik18406@iiitd.ac.in>
2020-06-14 09:56:20 +0100

    Add files via upload

---
+++ maalik_mail_1.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+read -p "Your email: " E
+echo -ne "Your passwd: "
+read -s P
+echo
+read -p "Recipient's email: " R
+read -p "No. of spam msgs: " L
+echo
+echo "initiating..."
+echo "Encoding the credentials in base64..."
+A="\00"
+F=$A$E$A$P
+C=$(echo -ne $F | base64)
+
+for (( i = 1; i <= $L; i++ )) 
+#for i in {1.."$L"}
+do
+	trash=`tr -dc A-Za-z0-9 </dev/urandom | head -c 20`
+	./maalik_mail_2.sh "$trash" "$i" "$C" "$R"
+	sleep 2
+done
---
+++ maalik_mail_2.sh
@@ -0,0 +1,39 @@
+#!/usr/bin/expect
+
+set trash "[lindex $argv 0]"
+set i "[lindex $argv 1]"
+set cred "[lindex $argv 2]"
+set R "[lindex $argv 3]"
+
+set timeout 10
+spawn openssl s_client -connect smtp.gmail.com:465 -crlf -ign_eof 
+
+expect "220" {
+send "EHLO localhost\n"
+
+expect "250" {
+send "AUTH PLAIN $cred\n"
+
+expect "235" {
+send "MAIL FROM: <ritikmaalik2000@gmail.com>\n"
+
+expect "250" {
+send "RCPT TO: <$R>\n"
+
+expect "250" {
+send "DATA\n"
+
+expect "354" {
+send "Subject: Test Spam #$i\n\n"
+send "Some trash msg - $trash \n"
+send "\n.\n"
+
+expect "250" {
+send "quit\n"
+            }
+          }
+        }
+      }
+    }
+  }
+}
